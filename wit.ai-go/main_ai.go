5c0dbec20a2c43fc525b26ac62229fe4fe9a5660
Mats Skjaervik <matses15@student.uia.no>
2017-04-25 19:27:16 +0200

    ICA-06

---
+++ wit.ai-go/main_ai.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+
+    witai "github.com/meinside/wit.ai-go"
+)
+
+const (
+    Token = "553XNQUSZEO7E3XXVMOSYMDW55YKX75R"
+)
+
+func main(){
+c := witai.NewClient(Token)
+
+//c.Verbose = false
+c.Verbose = true // for verbose messages
+
+// speech
+if result, err := c.QuerySpeechMp3("test3.mp3", nil, "", "", 1); err == nil {
+  fmt.Printf("query speech result: %+v\n", result)
+} else {
+  fmt.Printf("%s\n", err)
+}
+}
